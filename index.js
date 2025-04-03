28765e6d53d62277d4c7631441f2bbd69e94d24b
dvmkey <42848512+dvmkey@users.noreply.github.com>
2025-04-02 22:03:42 -0400

    This might work.  I think I forgot my start command in the yaml.  Let's see.

--- index.js
+++ index.js
@@ -4,11 +4,6 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
-
-app.get('/', (req, res) => {
-    res.send('Hello from Cloud Run!');
-  });
-/*
 // Serve static files with correct MIME types
 const staticPath = path.join(__dirname, 'scam-scam/dist');
 console.log(`Serving static files from: ${staticPath}`); // Add this line
@@ -27,7 +22,8 @@ app.get('{/*path}', (req, res) => {
   console.log(`Serving index.html from: ${indexPath}`); // add this line.
   res.sendFile(indexPath);
 });
-*/
+
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
