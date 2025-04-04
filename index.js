1a2bfe09c821b74e1850cc2ad31d296e203a8c2d
dvmkey <42848512+dvmkey@users.noreply.github.com>
2025-04-03 23:36:14 -0400

    Commented out basically everything that makes the app work

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const authRouter = require('./route/authRoute');
 const app = express();
 const PORT = process.env.APP_PORT || 8080;
 
-// Serve static files with correct MIME types
-const staticPath = path.join(__dirname, 'scam-scam/dist');
-console.log(`Serving static files from: ${staticPath}`); // Add this line
-
+// Uncomment everything between this and "catch all" if you want things to work
+//const staticPath = path.join(__dirname, 'scam-scam/dist');
+//console.log(`Serving static files from: ${staticPath}`); // Add this line
+/*
 app.use(express.static(staticPath, {
   setHeaders: (res, filePath) => {
     if (filePath.endsWith('.js')) {
@@ -18,13 +18,27 @@ app.use(express.static(staticPath, {
     }
   },
 }));
-
+*/
 // Catch-all route to serve index.html for React Router
+
+/* 
+
+Uncommenting the following code block will get the program to run just like it did before.
+However, the urls for this project need to be reworked.  The way it does page handling now
+prevents me from doing on server testing. 
+
+I could probably figure out how to make it work but that's beyond the scope of what I am
+responsible for at the moment and I really only care about getting the api's to work.
+
+*/
+
+/*
 app.get('{/*path}', (req, res) => {
   const indexPath = path.join(__dirname, 'scam-scam/dist/index.html');
   console.log(`Serving index.html from: ${indexPath}`); // add this line.
   res.sendFile(indexPath);
 });
+*/
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
@@ -32,11 +46,11 @@ app.listen(PORT, () => {
 
 // Additional Routes
 
-app.get('/rest', (req,res) => {
+app.get('/', (req,res) => {
   res.status(200).json({
     status: 'success',
     message: 'Routed Successfully',
   })
 });
 
-app.use('/api/v1/auth', authRouter);
+app.use('api/v1/auth', authRouter);
