da9e11ab484abf1e63747ba719ea59bbd9a3aeae
dvmkey <42848512+dvmkey@users.noreply.github.com>
2025-04-04 00:03:08 -0400

    Reorganized some things, fixed a syntax error in index.

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 require('dotenv').config({path: `${process.cwd()}/.env`});
 
 const express = require('express');
-const path = require('path');
+//const path = require('path');
 const authRouter = require('./route/authRoute');
 
 const app = express();
 const PORT = process.env.APP_PORT || 8080;
 
+app.get('/', (req,res) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'Routed Successfully',
+  })
+});
+
+// Additional Routes
+
+app.use('/api/v1/auth', authRouter);
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+
 // Uncomment everything between this and "catch all" if you want things to work
 //const staticPath = path.join(__dirname, 'scam-scam/dist');
 //console.log(`Serving static files from: ${staticPath}`); // Add this line
+
 /*
 app.use(express.static(staticPath, {
   setHeaders: (res, filePath) => {
@@ -19,6 +35,7 @@ app.use(express.static(staticPath, {
   },
 }));
 */
+
 // Catch-all route to serve index.html for React Router
 
 /* 
@@ -40,17 +57,4 @@ app.get('{/*path}', (req, res) => {
 });
 */
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
 
-// Additional Routes
-
-app.get('/', (req,res) => {
-  res.status(200).json({
-    status: 'success',
-    message: 'Routed Successfully',
-  })
-});
-
-app.use('api/v1/auth', authRouter);
