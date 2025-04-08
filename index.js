87cb89fc4310929e32e990254481fee431d2fc74
Jask <jasroke@gmail.com>
2025-04-07 22:48:36 -0400

    undo

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.listen(PORT, () => {
 const staticPath = path.join(__dirname, 'scam-scam/dist');
 console.log(`Serving static files from: ${staticPath}`); // Add this line
 
-
+/*
 app.use(express.static(staticPath, {
   setHeaders: (res, filePath) => {
     if (filePath.endsWith('.js')) {
@@ -37,7 +37,7 @@ app.use(express.static(staticPath, {
     }
   },
 }));
-
+*/
 
 // Catch-all route to serve index.html for React Router
 
