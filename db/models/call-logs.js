5f0ff1c6e70fc1d7bcf5cb65a6e4e32bd3d066aa
dvmkey <42848512+dvmkey@users.noreply.github.com>
2025-04-07 23:17:39 -0400

    Added a call logs model

---
+++ db/models/call-logs.js
@@ -0,0 +1,32 @@
+const {Datatypes } = require(sequelize)
+
+const sequelize = require("../../config/database");
+
+module.exports = sequelize.define(
+    'call_log', {
+        user: {
+            allowNull: false,
+            type: Datatypes.INTEGER,
+        },
+        id: {
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+            type: Datatypes.INTEGER
+        },
+        availability: {
+            type: Datatypes.STRING
+        },
+        notes: {
+            type: Datatypes.STRING
+        },
+        transcript: {
+            type: Datatypes.STRING
+        }
+    },
+    {
+        paranoid: true,
+        freezeTableName: true,
+        modelName: 'call_log',
+    }
+)
