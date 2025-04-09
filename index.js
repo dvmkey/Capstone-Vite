151137345d6f9cdf032929d5b56d27ff2e198490
Jask <jasroke@gmail.com>
2025-04-09 14:02:09 -0400

    fix route

--- index.js
+++ index.js
@@ -37,7 +37,10 @@ app.use(express.static(staticPath, {
     }
   }
 }));
-
+app.get(/.*/, (req, res) => {
+  console.log('Fallback route hit for:', req.url);
+  res.sendFile(path.join(staticPath, 'index.html'));
+});
 
 
 
